3042a283b4966f7f27311876d78060c0df4f57d0
aezi <30462422+minichest@users.noreply.github.com>
2022-02-14 20:38:05 +0000

    dot

--- first-legs/js/ben-ajx.js
+++ first-legs/js/ben-ajx.js
@@ -17,12 +17,12 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("ben-ajx").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("ben-ajx").innerHTML = "Tie Has Begun!";
+    document.getElementById("countdown").innerHTML = "Tie Has Begun!";
   }
 }, 1000);
